a521464a33971acf383aa7fb552b42e708da76ba
Gilberto Gonzalez <gilberto.gonzalez@supernal.aero>
2024-09-20 22:19:31 -0700

    applying filter

--- main.cpp
+++ main.cpp
@@ -12,13 +12,14 @@ std::string gstreamer_pipeline (int capture_width, int capture_height, int displ
 int main()
 {
     int w = 1920, h = 1080;
+    int d_w = 1280, d_h = 720;
     float fps = 30.0;
     std::string pipeline = gstreamer_pipeline(
-        w, // capture_width
-        h,  // capture_height
-        w, // display_width
-        h,  // display_height
-        fps,   // framerate
+        w,    // capture_width
+        h,    // capture_height
+        d_w,  // display_width
+        d_h,  // display_height
+        fps,  // framerate
         0     // flip_method
     );
  
@@ -33,7 +34,13 @@ int main()
     cv::VideoWriter writer;
     int codec = cv::VideoWriter::fourcc('M', 'P', '4', 'V');
     std::string filename = "./live.mp4";
-    writer.open(filename, codec, fps, cv::Size(w, h), true);
+    writer.open(filename, codec, fps, cv::Size(d_w, d_h), true);
+
+    // Create CUDA Gaussian filter
+    cv::Ptr<cv::cuda::Filter> gaussianFilter = cv::cuda::createGaussianFilter(CV_8UC3, CV_8UC3, cv::Size(11, 11), 0);
+
+    cv::Mat frame, blurred_frame;
+    cv::cuda::GpuMat d_frame, d_blurred_frame;
 
     cv::Mat img;
     for (int i = 0; i < 100; i++) {
@@ -42,7 +49,19 @@ int main()
             break;
         }
 
-        writer.write(img);
+        // Upload frame to GPU
+        d_frame.upload(img);
+
+        // Apply Gaussian blur using CUDA
+        gaussianFilter->apply(d_frame, d_blurred_frame);
+
+        // Download processed frame from GPU
+        d_blurred_frame.download(blurred_frame);
+
+
+        cv::Mat save_img = blurred_frame;
+        resize(blurred_frame, save_img, cv::Size(d_w, d_h));
+        writer.write(save_img);
 	
         int keycode = cv::waitKey(1) & 0xff; 
         if (keycode == 27) break;
