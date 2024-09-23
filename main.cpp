232aa40338b191783730e723ea85a4efe35d166f
Gilberto Gonzalez <gilberto.gonzalez@supernal.aero>
2024-09-22 21:04:42 -0700

    integrated motion detection

--- main.cpp
+++ main.cpp
@@ -1,7 +1,18 @@
 // reference: https://github.com/JetsonHacksNano/CSI-Camera/blob/master/simple_camera.cpp
 
 #include <opencv2/opencv.hpp>
-#include <opencv2/cudafilters.hpp>
+
+// Constants
+const int MASK_THRESHOLD = 25;
+const float PMS_THRESHOLD = 0.35;
+const cv::Size BLUR_SIZE(3, 3);
+const cv::Size DILATION_SIZE(7, 7);
+const bool SAVE_IMAGES = false;
+
+struct Box {
+    int x, y, w, h;
+    int area;
+};
 
 std::string gstreamer_pipeline (int capture_width, int capture_height, int display_width, int display_height, int framerate, int flip_method) {
     return "nvarguscamerasrc ! video/x-raw(memory:NVMM), width=(int)" + std::to_string(capture_width) + ", height=(int)" +
@@ -10,8 +21,67 @@ std::string gstreamer_pipeline (int capture_width, int capture_height, int displ
            std::to_string(display_height) + ", format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink";
 }
 
-int main()
-{
+std::vector<Box> detect_motion_clusters(cv::Mat& gray_prev_frame, cv::Mat& gray_frame) {
+    cv::Mat diff_frame, thresh_frame, dilated_frame;
+
+    // Compute abs diff and apply threshold
+    cv::absdiff(gray_prev_frame, gray_frame, diff_frame);
+    cv::threshold(diff_frame, thresh_frame, MASK_THRESHOLD, 255, cv::THRESH_BINARY);
+
+    // Dilate thresholded image
+    cv::Mat element = cv::getStructuringElement(cv::MORPH_RECT, DILATION_SIZE);
+    cv::dilate(thresh_frame, dilated_frame, element);
+
+    // Find connected components (clusters)
+    cv::Mat labels, stats, centroids;
+    int num_labels = cv::connectedComponentsWithStats(dilated_frame, labels, stats, centroids, 8, CV_32S);
+
+    std::vector<Box> bounding_boxes;
+    for (int i = 1; i < num_labels; i++) {
+        int area = stats.at<int>(i, cv::CC_STAT_AREA);
+        Box box{
+            stats.at<int>(i, cv::CC_STAT_LEFT),
+            stats.at<int>(i, cv::CC_STAT_TOP),
+            stats.at<int>(i, cv::CC_STAT_WIDTH),
+            stats.at<int>(i, cv::CC_STAT_HEIGHT),
+            area
+        };
+        bounding_boxes.push_back(box);
+    }
+    return bounding_boxes;
+}
+
+// reference: https://medium.com/@itberrios6/introduction-to-motion-detection-part-1-e031b0bb9bb2
+void non_max_suppression(std::vector<Box>& boxes, float thresh) {
+    // Remove bboxes that have high Intersection Over Union (IoU)
+    for (size_t i = 0; i < boxes.size(); i++) {
+        for (size_t j = i + 1; j < boxes.size(); ) {
+            float intersection_width = std::max(0, std::min(boxes[i].w + boxes[i].x, boxes[j].w + boxes[j].x) - std::max(boxes[i].x, boxes[j].x));
+            float intersection_height = std::max(0, std::min(boxes[i].h + boxes[i].y, boxes[j].w + boxes[j].y) - std::max(boxes[i].y, boxes[j].y));
+            float intersection = intersection_width * intersection_height;
+            float total_union = (boxes[i].area + boxes[j].area) - intersection;
+
+            float iou = 0.0f;
+            if (total_union > 0) {
+                iou = intersection / total_union;
+            }
+
+            // Check IoU threshold
+            if (iou > thresh) {
+                if (boxes[i].area > boxes[j].area) {
+                    boxes.erase(boxes.begin() + j);
+                } else {
+                    boxes.erase(boxes.begin() + i);
+                    break; // Break since box i was removed
+                }
+            } else {
+                j++; // Only increment j if no box was removed
+            }
+        }
+    }
+}
+
+int main() {
     int w = 1920, h = 1080;
     int d_w = 1280, d_h = 720;
     float fps = 30.0;
@@ -37,37 +107,54 @@ int main()
     std::string filename = "./live.mp4";
     writer.open(filename, codec, fps, cv::Size(d_w, d_h), true);
 
-    // Create CUDA Gaussian filter
-    cv::Ptr<cv::cuda::Filter> gaussianFilter = cv::cuda::createGaussianFilter(CV_8UC3, CV_8UC3, cv::Size(11, 11), 0);
+    cv::Mat gray_prev_frame, gray_frame;
 
-    cv::Mat frame, blurred_frame;
-    cv::cuda::GpuMat d_frame, d_blurred_frame;
+    int frame_count = 0;
+    while (true) {
+        cv::Mat frame;
+        cap >> frame;
+        if (frame.empty()) break;
 
-    cv::Mat img;
-    for (int i = 0; i < 100; i++) {
-    	if (!cap.read(img)) {
-            std::cout<<"Capture read error"<<std::endl;
-            break;
+        // Convert current frame to grayscale and blur it
+        cv::cvtColor(frame, gray_frame, cv::COLOR_BGR2GRAY);
+        cv::GaussianBlur(gray_frame, gray_frame, BLUR_SIZE, 0);
+
+        if (!gray_prev_frame.empty()) {
+            // Compute contours
+            std::vector<Box> boxes = detect_motion_clusters(gray_prev_frame, gray_frame);
+
+            // Apply non-maximal suppression to reduce noisy bboxs
+            non_max_suppression(boxes, PMS_THRESHOLD);
+
+            cv::Rect bbox;
+            for (const auto& box : boxes) {
+                cv::Rect bbox(box.x, box.y, box.w, box.h);
+                // Draw bbox
+                cv::rectangle(frame, bbox, cv::Scalar(0, 255, 0), 2);
+            }
+
+            // cv::Mat save_img = blurred_frame;
+            // cv::resize(blurred_frame, save_img, cv::Size(d_w, d_h));
+            // writer.write(save_img);
+
+            // Show
+            cv::imshow("Result", frame);
+            cv::waitKey(1);
+
+            if (SAVE_IMAGES) {
+                std::stringstream filename;
+                filename << "/imgs/frame_" << std::setw(4) << std::setfill('0') << frame_count << ".jpg";
+                cv::imwrite(filename.str(), frame);
+                frame_count++;
+            }
         }
 
-        // Upload frame to GPU
-        d_frame.upload(img);
-
-        // Apply Gaussian blur using CUDA
-        gaussianFilter->apply(d_frame, d_blurred_frame);
-
-        // Download processed frame from GPU
-        d_blurred_frame.download(blurred_frame);
-
-        cv::Mat save_img = blurred_frame;
-        cv::resize(blurred_frame, save_img, cv::Size(d_w, d_h));
-        writer.write(save_img);
-	
-        int keycode = cv::waitKey(1) & 0xff; 
-        if (keycode == 27) break;
+        // Update prev_frame
+        gray_prev_frame = gray_frame.clone();
     }
 
     cap.release();
-    writer.release();
+    cv::destroyAllWindows();
+
     return 0;
 }
